173faf07f360fc148916aba6a883668a1d6e4480
handelsr <handelsr@hotmail.es>
2024-07-16 14:20:50 -0400

    .

--- public/js/consultation_by_day.js
+++ public/js/consultation_by_day.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket('ws://localhost:8080');
+const ws = new WebSocket(`ws://${window.location.hostname}:8080`);
 
 ws.onopen = () => {
     console.log('Conexión establecida con el servidor WebSocket.');
